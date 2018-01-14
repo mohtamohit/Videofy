87e46e2960345ccbc79cb05ddb551d8413a12529
Suryaveer <ee150002037@iiti.ac.in>
2018-01-14 10:13:32 +0530

    Version 1.0
    A product of 3 days of hard work at wittyhacks.

---
+++ witty/index.php
@@ -0,0 +1,242 @@
+<?php
+error_reporting(0);
+function connect($data){
+  $host = "127.0.0.1";
+  $port = 2000;
+
+  $output=$data;
+
+  $socket1 = socket_create(AF_INET, SOCK_STREAM,0) or die("Could not create socket\n");
+
+  socket_connect ($socket1 , $host,$port ) ;
+
+  socket_write($socket1, $output, strlen ($output)) or die("Could not write output\n");
+
+  socket_close($socket1);
+  // echo '<script type="text/javascript">location.href = "inde.php";</script>';
+}
+
+function imgshow($path,$id){
+    echo '<img src="'.$path.'" id="'.$id.'" height="220" width="360">';
+}
+
+function textshow($text,$id){
+  // echo $text;
+  
+  echo  '<label for="textarea">'.($id-1).'.</label>';
+  echo  '<textarea name="'.$id.'" rows="4" cols="50" class="edit">'.$text.'</textarea>';
+}
+
+function edit_box(){
+      // Execute the python script with the JSON data
+      $result = file_get_contents("data.json");
+      //print($result);
+     // Decode the result
+
+    $resultData = json_decode($result, true);
+    // This will contain: array('status' => 'Yes!')
+    // var_dump($resultData);
+    // imgshow($resultData[1][1],0);
+    // echo $resultData[1][1];
+      for($i=2;$i<sizeof($resultData);$i++){
+        // echo $resultData[$i][1]+'<br>';
+        if($resultData[$i][0]=='img'){
+          imgshow($resultData[$i],$i);
+        }
+        else{
+          textshow($resultData[$i],$i);
+        }
+
+      }
+}
+function transcript(){
+  echo readfile("f1.txt");
+}
+?>
+
+<?php
+    $result = file_get_contents("data.json");
+    $resultData = json_decode($result, true);
+  if(isset($_POST['edit']))  
+  {
+    
+    connect("sdf");
+    // echo '<script language="javascript">';
+    // // echo 'alert("Wait for 10 seconds....")';
+    // echo '</script>';
+      $result = file_get_contents("data.json");
+      print($result);
+     // Decode the result
+
+    $resultData = json_decode($result, true);
+    for($i=2;$i<sizeof($resultData);$i++){
+        // echo $resultData[$i][1]+'<br>';
+        if($resultData[$i]=='img'){
+          // imgshow($resultData[$i][1],$i);
+        }
+        else{
+          echo $resultData[$i]=$_POST[$i];
+        }
+
+      }
+      echo $resultData;
+      $fp = fopen('data.json', 'w');
+      fwrite($fp, json_encode($resultData));
+      fclose($fp);
+      echo '<script type="text/javascript">location.href = "redirect.php";</script>';
+}
+ if(isset($_POST['url'])) {
+      $url = $_POST["url"];
+      $resultData =array("first",$url);
+      // $resultData = $url;
+      $fp = fopen('data.json', 'w');
+      fwrite($fp, json_encode($resultData));
+      fclose($fp);
+      connect("123");
+      echo '<script type="text/javascript">location.href = "redirect.php";</script>';
+ }
+
+?>
+
+<!DOCTYPE html> 
+<html> 
+<head>
+  <title>Videos</title>
+    <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+   
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="mdl/material.min.css">
+  <script src="mdl/material.min.js"></script>
+      <!--Import jQuery before materialize.js-->
+    
+</head>
+<body> 
+   <nav class="nav-extended">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo" style="margin-left: 2em;margin-top: 10px;">Video<b>fy</b></a>
+       <form method="POST">
+        <input type="text" name="url" placeholder="Link" style="background-color: white;color: black;margin-top: 20px;margin-left: 12em;width: 25em;font-size: 1.5em;padding-left: 10px;border-radius: 5px">
+        <a href="#" class="brand-logo" style="margin-left: 3em;margin-top: 10px;font-size: 1.5em">Made for <b>#Wittyhacks </b></a>
+        <img src="https://cdn.wittyfeed.com/assets_main_new/images/wittyfeed_new_update.svg" class="logo_new" alt="wittyfeed-beyond-stories" style="margin-left: 26.5em;width: 100px">
+      </form>
+    </div>
+  </nav>
+
+<div style="background-color: #eeeeee ;
+          margin-left:0;margin:2.5em;
+          float: left;width: 60%;
+          padding: 5px;
+          margin-bottom: 20px;
+          box-shadow: 1px 2px 20px grey"> 
+  
+  <video id="video1" width="770" height="405" controls >
+    <source src="test.mp4" type="video/mp4">
+    <source src="test.ogg" type="video/ogg">
+  </video>
+</div>
+
+
+
+  <form action="" method="POST"> 
+    <div style="background-color: #f0f0f0;
+                margin-top:2.5em;
+                margin-right: 2.5em;
+                width: 31%;   
+                float: right;
+                 box-shadow: 1px 2px 20px grey;
+                height: 525px">
+
+      <div style="margin-left: 9em;
+                  font-size: 1.2em;
+                  font-style: bold
+                  padding-top:2em">
+         <p ><b>Frame No.</b></p>
+      </div>
+
+      <div style="
+                float: right;
+                overflow-y: auto;
+                padding: 1.5em;
+                height: 490px;">
+        <?php edit_box();?>
+      </div>
+
+      <div>
+         <!-- <button id="edit"  name="edit"></button> -->
+         <button class="waves-effect waves-light btn" id="edit" name="edit" type="submit" style="width: 100%"><i class="material-icons right"></i>Edit video</button>
+      </div>
+    </div>
+    
+  </form>
+<u><a href="<?php echo $resultData[1]; ?>" style="
+                                          font-size: 1.5em;
+                                          margin-left: 1.5em;
+                                          color: grey;
+                                          margin-bottom:1em;"><?php echo $resultData[2]; ?></a></u>
+
+<div style="background-color: #eeeeee ;
+          margin-left:0;margin:2.5em;
+          margin-top: 20px;
+          float: left;width: 60%;
+          padding: 5px;
+          box-shadow: 1px 2px 20px grey"> 
+
+          <a class="waves-effect waves-light btn" href="test.mp4"><i class="material-icons right">file_download</i>Download</a>
+          <a class="waves-effect waves-light btn" style="background-color: #3b5998"><i class="material-icons right">share</i>facebook</a>
+
+
+      <ul class="collection with-header" style="margin: 0;margin-top: 5px">
+        <li class="collection-header"><h5 style="margin:0">Transcript</h5></li>
+        <li class="collection-item"><?php transcript();?></li>
+      </ul>
+</div>
+
+
+
+</body> 
+</html>
+
+<script> 
+var myVideo = document.getElementById("video1"); 
+
+function playPause() { 
+    if (myVideo.paused) 
+        myVideo.play(); 
+    else 
+        myVideo.pause(); 
+} 
+
+function makeBig() { 
+    myVideo.width = 560; 
+} 
+
+function makeSmall() { 
+    myVideo.width = 320; 
+} 
+
+function makeNormal() { 
+    myVideo.width = 420; 
+} 
+
+</script> 
+<style type="text/css">
+  .edit{
+      background-color: #fafafa;
+      height: 5em;
+      margin-bottom: .5em;
+      border: 1px solid #616161;
+      border-radius: 0;
+      outline: none;
+      height: 4rem;
+      width: 100%;
+      font-size: 1rem;
+      color: grey;
+      margin: 0 0 20px 0;
+      box-shadow: 1px 2px 10px grey;
+  }
+</style>
---
+++ witty/temp/test3.py
@@ -0,0 +1,168 @@
+import urllib3
+import urllib
+import json
+import nltk
+import sumy
+import os
+import numpy as np
+from bs4 import BeautifulSoup
+from sumy.parsers.plaintext import PlaintextParser 
+from sumy.nlp.tokenizers import Tokenizer 
+from sumy.summarizers.lex_rank import LexRankSummarizer 
+from moviepy.editor import *
+from moviepy.video.tools.drawing import color_gradient
+
+
+# RESOLUTION of the Clip
+
+w = 960
+h = 506 # 9/16 screen
+moviesize = (w,h)
+
+#Function to download images from the webpage
+
+def download_web_image(url, i):
+    full_name = os.getcwd() + "/images/" + str(i) +".jpg"
+    urllib.request.urlretrieve(url,full_name)
+
+urllib3.disable_warnings()
+http = urllib3.PoolManager()
+
+#Input url
+
+url = 'https://www.wittyfeed.com/story/61677/benefits-of-doing-namaz'
+response = http.request('GET', url)
+soup = BeautifulSoup(response.data, "lxml")
+
+f1 = open("f1.txt","w+")
+
+#Implementing BeautifulSoup
+temp = soup.title.text + "\n"
+para = soup.find_all('p')
+imgs = soup.find_all('img')
+
+
+numImages= 1
+
+for i in range(len(imgs)):
+    if (imgs[i]['src'].find("assets") == -1 and imgs[i]['src'].find("wittyfeed") != -1):
+        download_web_image("https:" + imgs[i]['src'] + "\n", numImages)
+        numImages+=1
+
+for i in range(len(para)):
+    f1 = open("f1.txt", "w+")
+    f1.write(para[i].text + "\n")
+    f1.close()
+    parser = PlaintextParser.from_file("f1.txt", Tokenizer("english"))
+    summarizer = LexRankSummarizer()
+    summary = summarizer(parser.document, 2)
+    for sentence in summary:
+        if(len(str(sentence)) > 30):
+            temp += str(sentence) + "\n\n"
+
+f1 = open("f1.txt", "w+")
+f1.write(temp)
+f1.close()
+cnt = 0
+
+txt_list =  (temp.split("\n"))
+txt_list = [txt_list[i] for i in range(len(txt_list)) if len(txt_list[i])<=200 and len(txt_list[i])>=30]
+
+sentence = ""
+line = ""
+
+
+f = open("data.json","r")
+data = f.read()
+jsonObj = json.loads(data)
+# print(jsonObj['1'][0])
+
+justifiedList = []
+
+if(jsonObj[0]=='first'):
+    justifiedList.append("second")
+    justifiedList.append(url)
+    for sent in txt_list:
+        line = ""
+        for txt in sent.split(" "):
+            if len(line)+len(txt)+1<=50:
+                line = line +  txt + " "
+            else:
+                sentence = sentence +line + "\n" 
+                line = txt + " "
+        sentence = sentence + line 
+        justifiedList.append(sentence)
+        sentence = ""
+    with open('data.json', 'w') as outfile:
+        json.dump(justifiedList, outfile)
+else:
+    for i in range(1,len(jsonObj)):
+        x = str(i)
+        justifiedList.append(jsonObj[i])
+print(justifiedList)
+
+if(numImages==1):
+    print("No Images found in article")
+    exit()
+
+avgSentence = int(np.ceil(len(justifiedList)/(numImages-1)))
+totalSentence = len(justifiedList)
+print(avgSentence)
+
+
+clip_txt = []
+for sentence in justifiedList:
+    cl = TextClip(sentence, color='white',fontsize=25,  font='Roboto').margin(20,opacity = 0).set_pos(('center','bottom'))
+    clip_txt.append(cl)
+
+
+clip_img = []
+for i in range(1,numImages):
+    clp = ImageClip("images/"+str(i)+".jpg").resize(width = w, height = h)
+    clip_img.append(clp)
+
+witty = ImageClip("wittywall.jpeg").resize(width = w, height = h)
+
+cmp_list = []
+cmp_list.append(witty)
+time = 2
+sent_index = 0 
+
+for i in range(0, numImages-1):
+    cmp_list.append(clip_img[i].set_start(time))
+    # print(str(i))
+    time = time + 2
+    for k in range(avgSentence):
+        if(totalSentence - i*avgSentence >= (numImages-i-1)*(avgSentence-1)):
+            if(k==avgSentence-1):
+                break
+        cmp_list.append(clip_img[i].set_start(time))
+        cmp_list.append(clip_txt[sent_index].set_start(time))
+        time = time+2
+        sent_index += 1 
+
+final = CompositeVideoClip(cmp_list,size = moviesize)
+
+final.set_duration(time+2).write_videofile("test.avi", fps=3,codec="mpeg4",audio='123.mp3')
+
+# Load myHolidays.mp4 and select the subclip 00:00:50 - 00:00:60
+clip = VideoFileClip("test.avi").subclip(0,time+2)
+    # Reduce the audio volume (volume x 0.8))
+clip.write_videofile("test.mp4")
+
+
+# # # SCROLL THE TEXT IMAGE BY CROPPING A MOVING AREA
+
+#txt_speed = 20
+
+#fl = lambda gf,t : gf(t)[int(txt_speed*t):int(txt_speed*t+np.ceil(h)):]
+# moving_txt= clip_txt.fl(fl, apply_to=['mask'])
+
+
+# final  = CompositeVideoClip([
+#          witty,
+#          stars_darkened.set_start(2),
+#          moving_txt.set_pos(('center','bottom')).set_start(3)], #warped_txt
+#          size = moviesize)
+
+#stars_darkened = stars.fl_image(lambda pic: (0.7*pic).astype('int16'))
